f39f5b5b506cd59caa45146b84f7d3d444be4d46
GG <gikonyogitau1@gmail.com>
2022-03-22 22:34:03 +0300

    used jquery to calculate the total order

--- js/script.js
+++ js/script.js
@@ -1,15 +1,38 @@
-const small = document.getElementById("small")
-const medium = document.getElementById("medium");
-const large = document.getElementById("large");
-const pepperoni= document.getElementById("pepperoni");
-const blackOlives = document.getElementById("black-olives");
-const bacon = document.getElementById("bacon");
-const crispy = document.getElementById("crispy");
-const stuffed = document.getElementById("stuffed");
-const glutenFree = document.getElementById("gluten-free");
+const pizzaSize = document.getElementById("pizzaSize");
+// const medium = document.getElementById("medium");
+// const large = document.getElementById("large");
+const pepperoni = document.getElementById("pepperoni");
+const blackOlives = document.getElementById("blackOlives");
+const pizzaCrust = document.getElementById("pizzaCrust");
+// const stuffed = document.getElementById("stuffed");
+// const glutenFree = document.getElementById("gluten-free");
+var size, crust;
 
+toppings = 0;
+total = 0;
 
-document.addEventListener("submit" )
+$(function () {
+  $("#submit").on("click", function (event) {
+    event.preventDefault();
+    size = parseInt(pizzaSize.value);
 
+    crust = parseInt(pizzaCrust.value);
 
-
+    if (pepperoni.checked == true && blackOlives.checked == true) {
+      var p1 = document.getElementById("pepperoni").value;
+      var bO1 = document.getElementById("blackOlives").value;
+      toppings = parseInt(p1) + parseInt(bO1);
+    } else if (pepperoni.checked == true) {
+      var p2 = document.getElementById("pepperoni").value;
+      toppings = parseInt(p2);
+    } else if (blackOlives.checked == true) {
+      var bO2 = document.getElementById("blackOlives").value;
+      toppings = parseInt(bO2);
+    } else {
+      return alert("please select at least one topping");
+    }
+    total = size + crust + toppings;
+    console.log(total);
+    return total;
+  });
+});
