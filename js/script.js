1be1d9e0dc4ff03579c51114f4bf4f51ef9d6e7b
GG <gikonyogitau1@gmail.com>
2022-03-22 19:58:51 +0300

    added ids to all the dropdowns

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const small = document.getElementById("small")
+const medium = document.getElementById("medium");
+const large = document.getElementById("large");
+const pepperoni= document.getElementById("pepperoni");
+const blackOlives = document.getElementById("black-olives");
+const bacon = document.getElementById("bacon");
+const crispy = document.getElementById("crispy");
+const stuffed = document.getElementById("stuffed");
+const glutenFree = document.getElementById("gluten-free");
+
+
+
+
